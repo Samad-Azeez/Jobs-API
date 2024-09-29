d062cd05c17c9561f42418932fac4c17ba720f50
Samad-Azeez <azeezsamad2004@gmail.com>
2024-09-29 15:42:01 +0100

    feat: Add authentication middleware to verify token in the header of the request object

--- starter/controllers/auth.js
+++ starter/controllers/auth.js
@@ -2,6 +2,7 @@ import { User } from '../models/User.js';
 import { StatusCodes } from 'http-status-codes';
 import { BadRequestError, UnauthenticatedError } from '../errors/index.js';
 
+// Register a new user in the database and return a JWT token for the user to authenticate themselves
 const register = async (req, res) => {
   const user = await User.create(req.body);
   const token = user.createJWT();
@@ -9,21 +10,25 @@ const register = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
+// Authenticate a user by checking the email and password in the request body and return a JWT token for the user to authenticate themselves
 const login = async (req, res) => {
   const { email, password } = req.body;
 
+  // check if the email and password are provided
   if (!email || !password) {
     throw new BadRequestError('please provide email and password');
   }
 
   const user = await User.findOne({ email });
 
+  // check if the user exists
   if (!user) {
     throw new UnauthenticatedError('invalid credentials');
   }
 
   const isPasswordCorrect = await user.comparePassword(password);
 
+  // check if the password is correct
   if (!isPasswordCorrect) {
     throw new UnauthenticatedError('invalid credentials');
   }
---
+++ starter/middleware/authentication.js
@@ -0,0 +1,27 @@
+import { User } from '../models/User.js';
+import jsonwebtoken from 'jsonwebtoken';
+import { UnauthenticatedError } from '../errors/index.js';
+
+// Middleware to authenticate the user by verifying the token in the header of the request object
+const auth = async (req, res, next) => {
+  // get the token from the header
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    throw new UnauthenticatedError('Authentication invalid');
+  }
+
+  // get the token from the header
+  const token = authHeader.split(' ')[1];
+
+  try {
+    // verify the token
+    const payload = jsonwebtoken.verify(token, process.env.JWT_SECRET);
+
+    //attach the user to the job routes
+    req.user = { userId: payload.userId, name: payload.name };
+  } catch (error) {
+    // if the token is invalid or expired, throw an error
+    throw new UnauthenticatedError('Authentication invalid');
+  }
+};
