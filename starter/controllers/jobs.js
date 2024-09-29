3ccb4ad8feee3c045a7ad9e2bc7d36de1a09937e
Samad-Azeez <azeezsamad2004@gmail.com>
2024-09-29 16:39:33 +0100

    feat: Update Job model to include company, position, status, and createdBy fields

--- starter/controllers/jobs.js
+++ starter/controllers/jobs.js
@@ -1,19 +1,24 @@
+// get all jobs
 const getAllJobs = async (req, res) => {
   res.send('get all jobs');
 };
 
+// get a job
 const getJob = async (req, res) => {
   res.send('get jobs');
 };
 
+// create a job
 const createJob = async (req, res) => {
-  res.send('create job');
+  res.json(req.user);
 };
 
+// update a job
 const updateJob = async (req, res) => {
   res.send('update job');
 };
 
+// delete a job
 const deleteJob = async (req, res) => {
   res.send('delete job');
 };
---
+++ starter/models/Job.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const JobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, 'please provide a company name'],
+      maxlength: 50,
+    },
+    position: {
+      type: String,
+      required: [true, 'please provide a position'],
+      maxlength: 100,
+    },
+    status: {
+      type: String,
+      enum: ['interview', 'declined', 'pending'],
+      default: 'pending',
+    },
+    createdBy: {
+      // user id of the user who created the job
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'please provide a user id'],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// export the model
+export const jobModel = mongoose.model('Job', JobSchema);
