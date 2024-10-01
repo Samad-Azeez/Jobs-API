258b3ba17b3344bda68ca7b82324aa34cc860ed8
Samad-Azeez <azeezsamad2004@gmail.com>
2024-10-01 07:51:46 +0100

    refactor: Update error-handler middleware to handle duplicate value errors

--- starter/middleware/error-handler.js
+++ starter/middleware/error-handler.js
@@ -5,20 +5,25 @@ export const errorHandlerMiddleware = (err, req, res, next) => {
   // not needed but added for clarity to also output the error to the console 😛😛
   console.error(err);
 
+  // set default error object with default values if not provided by the error object
   let customError = {
     //set default
     StatusCode: err.StatusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || 'something went wrong try again later',
   };
 
+  // handle custom API error
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message });
   }
 
+  // handle mongoose duplicate key error
   if (err.code && err.code === 11000) {
     customError = {
       StatusCode: 400,
-      msg: `Duplicate value entered for ${err.KeyValue} field, please choose another value`,
+      msg: `Duplicate value entered for ${
+        Object.keys(err.keyValue)[0]
+      } field, please choose another value`,
     };
   }
 
